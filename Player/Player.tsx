ba2f9310cc37b5811ca67db771744bc0e773f740
CT <CT>
2024-04-25 23:54:49 +0000

    commit

--- Player/Player.tsx
+++ Player/Player.tsx
@@ -1,18 +1,17 @@
-
 import React, { useRef, useEffect, useState } from 'react';
 
 const Player = ({ audioElem, isPlaying, setisplaying }) => {
-  const clickRef = useRef();
-  const [progress, setProgress] = useState(0);
+  const clickRef = useRef<HTMLDivElement>(null);
+  const [progress, setProgress] = useState<number>(0);
 
   const PlayPause = () => {
     setisplaying(prevState => !prevState); // Toggle between playing and pausing
   };
 
-  const checkWidth = (e) => {
-    const rect = clickRef.current.getBoundingClientRect();
+  const checkWidth = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const rect = clickRef.current!.getBoundingClientRect();
     const offsetX = e.clientX - rect.left;
-    const width = clickRef.current.clientWidth;
+    const width = clickRef.current!.clientWidth;
     const percentage = (offsetX / width) * 100;
     audioElem.current.currentTime = (percentage / 100) * audioElem.current.duration;
   };
@@ -36,14 +35,14 @@ const Player = ({ audioElem, isPlaying, setisplaying }) => {
         <div className="controls flex items-center">
           {isPlaying ? (
             <div className="text-3xl text-white cursor-pointer" onClick={PlayPause}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
               </svg>
             </div>
           ) : (
             <div className="text-3xl text-white cursor-pointer" onClick={PlayPause}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
               </svg>
             </div>
           )}
@@ -57,3 +56,4 @@ const Player = ({ audioElem, isPlaying, setisplaying }) => {
 };
 
 export default Player;
+
