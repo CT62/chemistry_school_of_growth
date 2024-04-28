716d60ef6618a5db09e323e3562d337b581f780e
CT <CT>
2024-04-28 11:37:51 +0000

    commit

--- app/prisma/client.ts
+++ app/prisma/client.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
+declare const global: {
+    prisma?: PrismaClient;
+};
+
 let prisma: PrismaClient;
 
 if (!global.prisma) {
     global.prisma = new PrismaClient();
 }
-prisma = global.prisma;
+prisma = global.prisma!;
 
 export default prisma;
