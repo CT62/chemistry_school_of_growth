ab27c7953aeb7389f4229f7725c5961b2f600a16
CT <138801309+CT62@users.noreply.github.com>
2024-10-11 17:25:49 +0100

    Update Sale.tsx

--- components/Sale.tsx
+++ components/Sale.tsx
@@ -1,70 +1,65 @@
-import {useEffect} from "react";
-import Navbar from '@/components/Navbar'
-import prisma from '../prisma/client';
-import Sale from "@/components/Sale"
-import CourseCards from '@/components/CourseCards'
-import CourseSale from "@/components/courseSale";
-import calculateTimeLeft from "@/components/calculateTimeLeft"
-import { Prompt } from 'next/font/google'
+"use client";
+import calculateTimeLeft from "@/components/calculateTimeLeft";
+import React, { useState, useEffect } from "react";
 
-export default async function Courses(){
-  const courses = await prisma.course.findMany({
-    orderBy: {
-      courseId: 'asc'
+interface TimeLeft {
+  days: number;
+  hours: number;
+  minutes: number;
+}
+
+export default function Sale() {
+  const [saleInfo, setSaleInfo] = useState({ timeLeft: { days: 0, hours: 0, minutes: 0 }, saleType: "" });
+  const [isPopupShown, setIsPopupShown] = useState(false);
+  const [isClient, setIsClient] = useState(false); // Check if component is running on the client
+
+  useEffect(() => {
+    setIsClient(true); // Once mounted, set to true
+  }, []);
+
+  useEffect(() => {
+    if (isClient) {
+      const popupStatus = sessionStorage.getItem("popup") !== "false";
+      setIsPopupShown(popupStatus);
+      sessionStorage.setItem("popup", "true");
     }
-  });
-  
-  let time = calculateTimeLeft();
-  let audios =["/organic_chemistry_1.mp3","/organic_chemistry_2.mp3","/5th_year_revision_course_part_I.mp3","/experiment_course_part_1.mp3","/fuels_and_thermochemistry.mp3","/experiment_course_part_2.mp3","/ultimate_h1_guide.mp3"];
+  }, [isClient]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const { timeLeft = { days: 0, hours: 0, minutes: 0 }, saleType = "" } = calculateTimeLeft() || {};
+      setSaleInfo({ timeLeft, saleType });
+    }, 60000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const togglePopup = () => {
+    sessionStorage.setItem("popup", "false");
+    setIsPopupShown(false);
+  };
+
+  const { timeLeft, saleType } = saleInfo;
+
+  // Ensure this part only renders if we are on the client
+  if (!isClient) return null;
+
   return (
-  <div className="bg-[#1e42b0]">
-    <div aria-hidden="true" className="absolute inset-0 grid grid-cols-3 -space-x-52">
-      <div className="blur-[106px] h-32 bg-cyan-300"></div>
-      <div className="blur-[106px] h-56 bg-cyan-400"></div>
-      <div className="blur-[106px] h-32 bg-cyan-300"></div>
-    </div>
-
-    <h1 className="relative pt-16 font-bold text-5xl flex justify-center text-white">Courses</h1>
-    
-
-
-    <div className="relative pb-14 pt-2 font-semibold text-xl justify-center text-white flex justify-center">
-      {time.saleType !== undefined && (
-        <CourseSale />
+    <>
+      {timeLeft.days && isPopupShown && (
+        <div className="fixed bottom-4 w-full">
+          <div className="bg-blue-600 p-4 font-bold text-2xl text-white overflow-hidden flex justify-between">
+            <span>
+              {saleType}! 40% off all courses! Ends in: {timeLeft.days}:{("0" + timeLeft.hours).slice(-2)}:{("0" + timeLeft.minutes).slice(-2)}
+            </span>
+            <button onClick={togglePopup}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+        </div>
       )}
-    </div>
-
-    <div className="container mx-auto">
-      <div className="relative grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-4">
-        {courses.map((course, index) => (
-          <CourseCards
-            key={index}
-            fakeprice={course.fakeprice}
-            price={course.price}
-            time={course.time}
-            title={course.title}
-            points={course.points}
-            reviewsCount={course.reviewsCount}
-            stars={course.stars}
-            titledesc={course.titleDesc}
-            courseID={String(index)}
-            AudioURL={audios[index]}
-          />
-        ))}
-      </div>
-    </div>
-     <div className="relative pt-24 pb-0">
-      <div className="pt-8 px-2 pb-4 w-full bg-white item-center h-full">
-        <div className="text-center text-black font-bold text-3xl pb-4">Whats included in each course</div>
-        <li className="sm:flex sm:justify-center pb-2.5">Notes</li>
-        <li className="sm:flex sm:justify-center pb-2.5">Exam questions & solutions</li>
-        <li className="sm:flex sm:justify-center pb-2.5">Unlimited access to 1-on-1 questions and solutions by emailing/messaging me directly</li>
-        <li className="sm:flex sm:justify-center pb-2.5">On demand access to recordings for the duration of your Leaving Cert</li>
-        <li className="sm:flex sm:justify-center pb-2.5">Free video solutions to material covered outside of the course content</li>
-        <li className="sm:flex sm:justify-center pb-2.5">Tips on how to get a H1</li>
-      </div>
-    </div>
-  </div>
-
-);
+    </>
+  );
 }
